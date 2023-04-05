fd7f504d38d64aae6f27f03e9a9758c752354bc1
Jymapas <Jymapas@yandex.ru>
2023-04-05 17:27:27 +0300

    Первая рабочая версия от GhatGPT, правда не без ошибок.

---
+++ TimerExample/Models/TimerModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Timers;
+
+namespace TimerExample.Models;
+
+public class TimerModel
+{
+    private Timer _timer;
+    private int _secondsLeft;
+
+    public event EventHandler<int> TimeChanged;
+
+    public bool IsRunning { get; private set; }
+    public TimerModel()
+    {
+        _timer = new Timer(1000);
+        _timer.Elapsed += OnTimedEvent;
+        _secondsLeft = 60;
+    }
+
+    public void Start()
+    {
+        IsRunning = true;
+        _timer.Start();
+    }
+
+    public void Stop()
+    {
+        IsRunning = false;
+        _timer.Stop();
+    }
+
+    private void OnTimedEvent(object sender, ElapsedEventArgs e)
+    {
+        _secondsLeft--;
+        TimeChanged?.Invoke(this, _secondsLeft);
+
+        if (_secondsLeft == 0) Stop();
+    }
+}
---
+++ TimerExample/Views/TimerView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace TimerExample.Views;
+
+public partial class TimerView : Window
+{
+    public TimerView()
+    {
+        InitializeComponent();
+    }
+}
